6c16e789c623e977d232fd5d508a16c4ab732a50
Sangariusss <volodya.sangarius.boklakh@gmail.com>
2025-08-19 20:49:17 +0300

    feat: add password recovery page

---
+++ frontend/app/forgot-password/page.tsx
@@ -0,0 +1,22 @@
+import { Footer } from '@/components/Footer'
+import { Header } from '@/components/Header'
+import { PasswordRecoveryForm } from '@/components/PasswordRecoveryForm'
+import { LegalLinks } from '@/components/LegalLinks'
+
+export default function PasswordRecoveryPage() {
+  return (
+    <>
+      <main className="bg-white min-h-screen flex flex-col">
+        <Header />
+        <section className="flex-1 flex items-center justify-center">
+          <div className="max-w-md mx-auto text-center">
+            <h1 className="text-[2rem] font-normal mb-2">Password Recovery</h1>
+            <PasswordRecoveryForm />
+            <LegalLinks />
+          </div>
+        </section>
+      </main>
+      <Footer />
+    </>
+  )
+}
